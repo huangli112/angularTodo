b42a850f7d7a20c3242cdcb5ebc709a6aae22f43
黄丽 <1121103546@qq.com>
2020-08-03 09:21:17 +0800

    添加列表数据项

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,22 @@
 import { Component } from '@angular/core';
+//定义数据
+const todos = [{
+  id: 1,
+  title: '购物',
+  done: false
+},
+{
+  id: 2,
+  title: '吃饭 ',
+  done: true
+},
+{ 
+  id: 3,
+  title: '下班',
+  done: false
+},
+
+]
 
 @Component({
   selector: 'app-root',
@@ -6,5 +24,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoList';
+  public todos: {
+    id: number,
+    title: string,
+    done: boolean
+  }[] = todos
+  addTodo(event): void {
+    //获取事件文本框中的数据
+    let val = event.target.value
+    //获取事件数据 的最后一项
+    let lastInfo = this.todos[todos.length - 1]
+    if (!val.length) {
+      return //数据为空时返回none
+    }
+    this.todos.push({
+      //如果数组长度为空 则id值为1 否则最后一项的id值+1
+      id: lastInfo.id ? lastInfo.id + 1 : 1,
+      title: val,
+      done: false
+    })
+    //清除文本框
+    event.target.value = ''
+    console.log(this.todos)
+  }
+ 
 }
+
